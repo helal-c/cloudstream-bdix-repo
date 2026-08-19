57fb7f36b3a98f18bc7337a28c788f001cbce56c
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 14:08:40 +0600

    Migrate buildscript to plugins block in build.gradle.kts
    Removed the buildscript block and migrated to plugins block for Gradle configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,6 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // এই লাইনটি Cloudstream প্লাগিন ডাউনলোড সমস্যার ১০০% সমাধান করবে
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-    }
+plugins {
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 allprojects {
