e00310ae0b0c428bcede6f708d4d5db6e55ebc27
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 13:55:13 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
-plugins {
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.lagradost.cloudstream3.gradle") version "0.1.0" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        // এই লাইনটি Cloudstream প্লাগিন ডাউনলোড সমস্যার ১০০% সমাধান করবে
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
 }
+
 allprojects {
     repositories {
         google()
