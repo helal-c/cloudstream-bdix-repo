e29e6fbffe1f08530e528b598dbc4b8f6c70399e
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 00:57:47 +0600

    Update build.gradle.kts

--- XtremexTvProvider/build.gradle.kts
+++ XtremexTvProvider/build.gradle.kts
@@ -4,13 +4,6 @@ plugins {
     id("com.lagradost.cloudstream3.gradle")
 }
 
-cloudstream {
-    name = "XtremeX TV"
-    description = "Live TV Provider by Xtreme'x"
-    versionCode = 1
-    authors = listOf("Xtreme'x")
-}
-
 android {
     namespace = "com.xtremex.tv"
     compileSdk = 34
