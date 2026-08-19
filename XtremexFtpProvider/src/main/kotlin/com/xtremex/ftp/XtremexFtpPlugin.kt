2eb53baeaa83332e2630b2b321b13522911d91ec
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 02:22:49 +0600

    Update XtremexFtpPlugin.kt

--- XtremexFtpProvider/src/main/kotlin/com/xtremex/ftp/XtremexFtpPlugin.kt
+++ XtremexFtpProvider/src/main/kotlin/com/xtremex/ftp/XtremexFtpPlugin.kt
@@ -1,8 +1,8 @@
 package com.xtremex.ftp
 
 import android.content.Context
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.plugins.*
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class XtremexFtpPlugin : Plugin() {
