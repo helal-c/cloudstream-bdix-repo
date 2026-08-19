122bec4290bff0df6e2854351a00e19e31818738
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 20:37:57 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.lagradost.cloudstream3.gradle") version "master-SNAPSHOT" apply false
 }
 
 allprojects {
