dd5bf952e3a3bce14dff28fbc136e19d6445b493
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 13:35:39 +0600

    Update build.gradle.kts

--- XtremexFtpProvider/build.gradle.kts
+++ XtremexFtpProvider/build.gradle.kts
@@ -1,1 +1,19 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    // এখানে আপনার গিটহাব ইউজারনেম helal-c বসানো হয়েছে
+    setRepoUrl("https://raw.githubusercontent.com/helal-c/cloudstream-bdix-repo/builds")
+}
+
+android {
+    namespace = "com.xtremex.ftp"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+}
 
