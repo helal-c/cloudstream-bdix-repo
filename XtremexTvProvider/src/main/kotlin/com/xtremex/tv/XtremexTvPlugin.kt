c53f7854f1b7dd9c2f04a90bdcf1d83c2fb353f4
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 02:17:20 +0600

    Update XtremexTvPlugin.kt

--- XtremexTvProvider/src/main/kotlin/com/xtremex/tv/XtremexTvPlugin.kt
+++ XtremexTvProvider/src/main/kotlin/com/xtremex/tv/XtremexTvPlugin.kt
@@ -1,8 +1,8 @@
 package com.xtremex.tv
 
 import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.plugins.*
 
 @CloudstreamPlugin
 class XtremexTvPlugin : Plugin() {
