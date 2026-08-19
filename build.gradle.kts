1b8d16d871ffd433bee609644360dce22ea7eb7c
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 13:26:12 +0600

    Add plugins for Android library and Kotlin
    Added Android library and Kotlin plugins with versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,13 @@
+plugins {
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.lagradost.cloudstream3.gradle") version "0.1.0" apply false
+}
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
 
