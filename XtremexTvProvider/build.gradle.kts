b471502e960ce8c6160a147bed7de5679a9e5f54
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 14:16:55 +0600

    Remove cloudstream repo configuration
    Removed cloudstream configuration from build.gradle.kts

--- XtremexTvProvider/build.gradle.kts
+++ XtremexTvProvider/build.gradle.kts
@@ -4,11 +4,6 @@ plugins {
     id("com.lagradost.cloudstream3.gradle")
 }
 
-cloudstream {
-    // এখানে আপনার গিটহাব ইউজারনেম helal-c বসানো হয়েছে
-    setRepoUrl("https://raw.githubusercontent.com/helal-c/cloudstream-bdix-repo/builds")
-}
-
 android {
     namespace = "com.xtremex.tv"
     compileSdk = 34
@@ -16,4 +11,3 @@ android {
         minSdk = 21
     }
 }
-
