329a90712247ad02d2029d5d6d32694bf22ea2f1
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-19 13:41:42 +0600

    Implement XtremexFtpPlugin with main API registration

--- XtremexFtpProvider/src/main/kotlin/com/xtremex/ftp/XtremexFtpPlugin.kt
+++ XtremexFtpProvider/src/main/kotlin/com/xtremex/ftp/XtremexFtpPlugin.kt
@@ -1,1 +1,12 @@
+package com.xtremex.ftp
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class XtremexFtpPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(XtremexFtpProvider())
+    }
+}
 
