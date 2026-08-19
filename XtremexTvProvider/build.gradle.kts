f312eedc482d7d3ed00b2db382c94b48cb6bc2f6
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 02:56:59 +0600

    Update build.gradle.kts

--- XtremexTvProvider/build.gradle.kts
+++ XtremexTvProvider/build.gradle.kts
@@ -23,6 +23,5 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
-    implementation("com.github.recloudstream.cloudstream:library-android:-SNAPSHOT")
+    compileOnly("com.github.recloudstream:cloudstream:-SNAPSHOT")
 }
