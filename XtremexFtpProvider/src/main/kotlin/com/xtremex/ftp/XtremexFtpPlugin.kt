d7d88834318cbfc935e4267268167ed8234e773b
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 10:28:48 +0600

    Create XtremexFtpPlugin.kt

---
+++ XtremexFtpProvider/src/main/kotlin/com/xtremex/ftp/XtremexFtpPlugin.kt
@@ -0,0 +1,13 @@
+package com.xtremex.ftp
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class XtremexFtpPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(XtremexFtpProvider())
+    }
+}
