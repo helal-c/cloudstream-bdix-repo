a974307fd4fa17afd7585ea38a00a646dc432a3f
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 10:26:35 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,21 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
         maven("https://jitpack.io")
     }
+
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
+}
+
+plugins {
+    id("com.android.library") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.lagradost.cloudstream3.gradle") apply false
 }
 
 allprojects {
@@ -16,6 +24,51 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "org.jetbrains.kotlin.android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    android {
+        compileSdk = 35
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+            )
+
+            freeCompilerArgs.add(
+                "-Xskip-metadata-version-check"
+            )
+        }
+    }
+
+    dependencies {
+        add(
+            "cloudstream",
+            "com.lagradost:cloudstream3:pre-release"
+        )
+    }
+
+    cloudstream {
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "helal-c/cloudstream-bdix-repo"
+        )
     }
 }
 
