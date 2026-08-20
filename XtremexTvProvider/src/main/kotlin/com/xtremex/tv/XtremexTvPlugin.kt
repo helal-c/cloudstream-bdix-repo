8074149608dc7c699932f5b1b022165a8cd997a1
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 10:29:18 +0600

    Add XtremexTvPlugin class for Cloudstream integration

---
+++ XtremexTvProvider/src/main/kotlin/com/xtremex/tv/XtremexTvPlugin.kt
@@ -0,0 +1,13 @@
+package com.xtremex.tv
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class XtremexTvPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(XtremexTvProvider())
+    }
+}
