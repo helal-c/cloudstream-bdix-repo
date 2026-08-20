152c422d1654072dc0abe978334e18eaf54f90f8
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 09:48:49 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,7 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
-    }
+plugins {
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.lagradost.cloudstream3.gradle")
 }
 
 allprojects {
@@ -25,85 +12,6 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(
-    configuration: CloudstreamExtension.() -> Unit
-) = extensions
-    .getByName<CloudstreamExtension>("cloudstream")
-    .configuration()
-
-fun Project.android(
-    configuration: BaseExtension.() -> Unit
-) = extensions
-    .getByName<BaseExtension>("android")
-    .configuration()
-
-subprojects {
-
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(
-            System.getenv("GITHUB_REPOSITORY")
-                ?: "helal-c/cloudstream-bdix-repo"
-        )
-    }
-
-    android {
-        namespace = "recloudstream"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    tasks.withType<KotlinJvmCompile> {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-
-            freeCompilerArgs.addAll(
-                "-Xno-call-assertions",
-                "-Xno-param-assertions",
-                "-Xno-receiver-assertions"
-            )
-        }
-    }
-
-    dependencies {
-        val implementation by configurations
-
-        implementation(
-            "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
-        )
-
-        implementation(kotlin("stdlib"))
-
-        implementation(
-            "com.github.Blatzar:NiceHttp:0.4.11"
-        )
-
-        implementation(
-            "org.jsoup:jsoup:1.18.3"
-        )
-
-        implementation(
-            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
-        )
-
-        implementation(
-            "com.github.teamnewpipe:NewPipeExtractor:v0.25.2"
-        )
-    }
-}
-
-tasks.register<Delete>("clean") {
+task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
