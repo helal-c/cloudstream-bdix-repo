fe464098de4905d6f4243b16487d5ef43472de92
helal-c <137490078+helal-c@users.noreply.github.com>
2026-08-20 10:28:15 +0600

    Update build.gradle.kts

--- XtremexTvProvider/build.gradle.kts
+++ XtremexTvProvider/build.gradle.kts
@@ -1,29 +1,19 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.lagradost.cloudstream3.gradle")
+version = 1
+
+cloudstream {
+    description = "Xtreme'x BDIX Live TV"
+    authors = listOf("Helal Uddin")
+    status = 1
+    tvTypes = listOf("Live")
+    language = "bn"
 }
 
 android {
     namespace = "com.xtremex.tv"
-    compileSdk = 34
+
+    compileSdk = 35
+
     defaultConfig {
         minSdk = 21
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
-        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-    }
-}
-
-dependencies {
-    compileOnly("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
 }
